1b026b4922c1639539e18930d33b8b1caa4b187f
Tanuj Sharma <tanuj.sharma@tothenew.com>
2020-09-01 14:23:43 +0530

    Initial Commit

---
+++ PlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerDemo
+//
+//  Created by Tanuj Sharma on 01/09/20.
+//  Copyright © 2020 Tanuj Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
